c041644a535dac81ab619d082b17bc6600c7b714
sihyeong671 <bshlab671@naver.com>
2023-10-24 01:34:07 -0700

    wip

--- module/model.py
+++ module/model.py
@@ -36,4 +36,41 @@ class MLP(nn.Module):
     x = self.block2(x)
     x = self.regression_head(x)
     return x
+
+class MLP(nn.Module):
+  def __init__(self, input_dim, hidden_dim, output_dim, dropout_rate=0.3):
+    super().__init__()
+    self.input_dim = input_dim
+    self.hidden_dim = hidden_dim
+    self.output_dim = output_dim
+    self.dropout_rate = dropout_rate
+    
+    self.block1 = nn.Sequential(
+      nn.Linear(self.input_dim, self.hidden_dim),
+      nn.BatchNorm1d(self.hidden_dim),
+      nn.ReLU(inplace=True),
+      nn.Dropout(p=self.dropout_rate),
+      nn.Linear(self.hidden_dim, self.hidden_dim),
+      nn.BatchNorm1d(self.hidden_dim),
+      nn.ReLU(inplace=True),
+    )
+    
+    
+    self.block2 = nn.Sequential(
+      nn.Linear(self.hidden_dim, self.hidden_dim),
+      nn.BatchNorm1d(self.hidden_dim),
+      nn.ReLU(inplace=True),
+      nn.Dropout(p=self.dropout_rate),
+      nn.Linear(self.hidden_dim, self.hidden_dim),
+      nn.BatchNorm1d(self.hidden_dim),
+      nn.ReLU(inplace=True),
+    )
+
+    self.regression_head = nn.Linear(self.hidden_dim, self.output_dim)
+    
+  def forward(self, x):
+    x = self.block1(x)
+    x = self.block2(x)
+    x = self.regression_head(x)
+    return x
     
